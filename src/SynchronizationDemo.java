505d7fdf6aa9bd74094df746a727d0e5a2a1a6f4
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-21 01:52:57 +0530

    added

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -8,39 +8,28 @@ class Table {
     }
 }
 
-class MyThread1 extends Thread {
-    Table t;
-
-    MyThread1(Table t) {
-        this.t = t;
-    }
-
-    public void run() {
-        t.printTable(5);
-    }
-}
-
-class MyThread2 extends Thread {
-    Table t;
-
-    MyThread2(Table t) {
-        this.t = t;
-    }
-
-    public void run() {
-        t.printTable(100);
-    }
-}
-
 public class SynchronizationDemo {
+
     public static void main(String[] args) {
 
         Table obj = new Table();
 
-        MyThread1 t1 = new MyThread1(obj);
-        MyThread2 t2 = new MyThread2(obj);
+        Thread t1 = new Thread() {
+            public void run() {
+                obj.printTable(5);
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                obj.printTable(100);
+            }
+        };
 
         t1.start();
+        try { t1.join(); } catch (Exception e) {}
+
         t2.start();
+        try { t2.join(); } catch (Exception e) {}
     }
 }
