aceff97ae2dfa4d66490a459ba1337467d061b3a
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-21 01:44:08 +0530

    add

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,34 +1,30 @@
 class Table {
-    // TODO: synchronized method void printTable(int n)
-    // Loop from 1 to 5 (as per sample) or 10
-    // Print n * i + " "
-    // Handle InterruptedException (try-catch Thread.sleep(400))
-}
-
-class MyThread1 extends Thread {
-    Table t;
-    MyThread1(Table t) {
-        this.t = t;
-    }
-    public void run() {
-        // TODO: Call t.printTable(5)
-    }
-}
-
-class MyThread2 extends Thread {
-    Table t;
-    MyThread2(Table t) {
-        this.t = t;
-    }
-    public void run() {
-        // TODO: Call t.printTable(100)
+    synchronized void printTable(int n) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print((n * i) + " ");
+        }
+        System.out.println();
     }
 }
 
 public class SynchronizationDemo {
     public static void main(String[] args) {
-        // TODO: Create Table object
-        // TODO: Create MyThread1 and MyThread2 objects passing the table object
-        // TODO: Start both threads
+
+        Table obj = new Table();
+
+        Thread t1 = new Thread() {
+            public void run() {
+                obj.printTable(5);
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                obj.printTable(100);
+            }
+        };
+
+        t1.start();
+        t2.start();
     }
 }
