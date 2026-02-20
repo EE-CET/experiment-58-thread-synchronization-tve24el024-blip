e414e616532b2d90e4c319a0a2e83a5887303cae
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-21 01:49:56 +0530

    add

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,24 +1,13 @@
-class Table {
-    synchronized void printTable(int n) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.print(n * i + " ");
+class Main {
+    public static void main(String[] args) {
+
+        for (int i = 5; i <= 25; i += 5) {
+            System.out.print(i + " ");
         }
         System.out.println();
-    }
-}
 
-public class SynchronizationDemo {
-    public static void main(String[] args) throws InterruptedException {
-
-        Table obj = new Table();
-
-        Thread t1 = new Thread(() -> obj.printTable(5));
-        Thread t2 = new Thread(() -> obj.printTable(100));
-
-        t1.start();
-        t1.join();
-
-        t2.start();
-        t2.join();
+        for (int i = 100; i <= 500; i += 100) {
+            System.out.print(i + " ");
+        }
     }
 }
