a5c26615bb9d5f8b1cacad693dd0a0eb8faa1687
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-21 01:54:07 +0530

    add

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -10,26 +10,17 @@ class Table {
 
 public class SynchronizationDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Table obj = new Table();
 
-        Thread t1 = new Thread() {
-            public void run() {
-                obj.printTable(5);
-            }
-        };
-
-        Thread t2 = new Thread() {
-            public void run() {
-                obj.printTable(100);
-            }
-        };
+        Thread t1 = new Thread(() -> obj.printTable(5));
+        Thread t2 = new Thread(() -> obj.printTable(100));
 
         t1.start();
-        try { t1.join(); } catch (Exception e) {}
+        t1.join();   // force order
 
         t2.start();
-        try { t2.join(); } catch (Exception e) {}
+        t2.join();
     }
 }
